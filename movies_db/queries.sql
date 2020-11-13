6051415b960095303b9bdf2bb53475b41ebe835b
David Whitlatch <davidtwhitlatch@gmail.com>
2020-11-13 14:54:08 -0500

    updated solution

--- movies_db/queries.sql
+++ movies_db/queries.sql
@@ -18,15 +18,16 @@ JOIN users ON users_movies.user_id = users.user_id
 GROUP BY movies.title;
 
 -- List the names of directors along with the number of favorites that exist for all of the movies they've made, ordered by number of favorites descending.
-SELECT directors.name, COUNT(users.name) FROM movies
+SELECT directors.name, COUNT(*) FROM movies
 JOIN users_movies ON users_movies.movie_id = movies.movie_id
 JOIN users ON users_movies.user_id = users.user_id
 JOIN directors ON directors.director_id = movies.director_id
 GROUP BY directors.name
-ORDER BY COUNT(users.name) DESC;
+ORDER BY COUNT(*) DESC;
 
 -- List the user name, director name and favorite count of all of the user/director combinations (based on the users_movies table).
-SELECT directors.name, COUNT(users.name) FROM movies
+SELECT directors.name, users.name, COUNT(*) FROM movies
 JOIN users_movies ON users_movies.movie_id = movies.movie_id
 JOIN users ON users_movies.user_id = users.user_id
-JOIN directors ON directors.director_id = movies.director_id;
+JOIN directors ON directors.director_id = movies.director_id
+GROUP BY directors.name, users.name;
